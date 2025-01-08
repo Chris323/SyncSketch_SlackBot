5cb6733cf888ce4ad3b153b5cbdd572a5072f5ef
Chris Ortega <chrisortega94@gmail.com>
2025-01-07 18:56:56 -0800

    create and test connection to ss api

--- SyncSketchBot/syncSketch.py
+++ SyncSketchBot/syncSketch.py
@@ -1,5 +1,21 @@
+from syncsketch import SyncSketchAPI
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 class SyncSketch:
-    def run_connection(self):
-        return
+    def __init__(self):
+        self.username = os.getenv("SYNCSKETCH_SCRIPT_USER_ACCOUNT")
+        self.api_key = os.getenv("SYNCSKETCH_SCRIPT_USER_APP_API")
+        self.s = SyncSketchAPI(self.username, self.api_key)
+
+#     Verify your credentials work
+#     def run_test(self):
+#         print(self.s.is_connected())
+#         Display your current user data
+#         print(s.get_current_user())
+    
+
+# testing = SyncSketch()
+# testing.run_test()
